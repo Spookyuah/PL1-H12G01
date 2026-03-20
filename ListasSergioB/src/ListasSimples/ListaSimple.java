0982a13c1c0a72aa8bb26ee51963482644f6dc61
Spookyuah <sergio.bonillo@edu.uah.es>
2026-03-20 23:31:23 +0100

    Update ListaSimple.java

--- ListasSergioB/src/ListasSimples/ListaSimple.java
+++ ListasSergioB/src/ListasSimples/ListaSimple.java
@@ -7,11 +7,11 @@ public class ListaSimple<T extends Comparable<T>>{
     public void add(T dato) {
         NodoSimple<T> nuevo = new NodoSimple<>(dato);
         if (primero == null) {
-            primero = nuevo;
+            primero = nuevo;    //Caso lista vacia
         } else {
             NodoSimple<T> aux = primero;
-            while (aux.siguiente != null) aux = aux.siguiente;
-            aux.siguiente = nuevo;
+            while (aux.siguiente != null) aux = aux.siguiente;  //Avanza hasta el ultimo nodo
+            aux.siguiente = nuevo;        //Enlaca el ultimo nodo con el nuevo dato
         }
         size++;
     }
@@ -29,8 +29,8 @@ public class ListaSimple<T extends Comparable<T>>{
         NodoSimple<T> act = primero, ant = null;
         while (act != null) {
             if (act.dato.compareTo(dato) == 0) {
-                if (ant == null) primero = act.siguiente;
-                else ant.siguiente = act.siguiente;
+                if (ant == null) primero = act.siguiente;     //Eliminar primero
+                else ant.siguiente = act.siguiente;        //Saltarse el nodo actual
                 size--;
                 return act.dato;
             }
